b587ae0a6166f3025e38584b20a3a1fd26698e54
Christoph Strasen <christoph.strasen.germany@gmail.com>
2019-02-28 15:56:02 +0000

    Adds aenc and adec symmetric encryption functions to bashrc

--- bashrc-additions.sh
+++ bashrc-additions.sh
@@ -22,3 +22,13 @@ gc() {
 
 	tmux new-window "$commit" \; split-window -dh "$diff"
 }
+
+aenc() {
+    #encrypt
+    gpg --s2k-mode 3 --s2k-count 65011712 --s2k-digest-algo SHA512 --s2k-cipher-algo AES256 --symmetric $1
+  }
+
+adec() {
+    #decrypt
+    gpg -d $1
+  }
